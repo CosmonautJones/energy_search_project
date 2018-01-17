29f02c58b66bea2d93aa29419e7a6a603bf5160e
CosmonautJones <travisj_jones@hotmail.com>
2018-01-17 17:43:00 -0600

    trying to manage deployement with api

--- src/components/EnergyPlans.js
+++ src/components/EnergyPlans.js
@@ -5,28 +5,57 @@ import Plan from './Plan';
 import { getPlans } from '../actions';
 import '../css_components/EnergyPlans.css';
 
+
+const apiURL =
+	'http://api.powertochoose.org/api/PowerToChoose/plans?zip_code=78364';
+
 class EnergyPlans extends Component {
-    render() {
-        return(
-            <div className="PlanPage">
-                <h1 className="PlanPage--title"> Shop. Compare. Choose. </h1>
-                <p className ="PlanList">
-                    {this.props.plans.map((plan, i) => {
+	constructor() {
+		super();
+		this.state = {
+			plans: []
+		};
+	}
+
+  getPlans() {
+    fetch(
+      apiURL,
+			{
+        headers: {
+          'Content-Type': 'application/json'
+				}
+			}
+		)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      this.setState({ plans: data.data });
+      console.log('state of Energy', this.state.plans);
+    });
+	};
+  
+  componentDidMount() {
+    this.getPlans();
+  }
+
+	render() {
+		return (
+			<div className="container1">
+				<div className="container2"><p className ="PlanList">
+                    {this.state.plans.map((plan, i) => {
                         return (
                             <Plan
                                 key={i} 
                                 index={i} 
-                                plan={plan} 
+                                data={plan} 
                             />
                         );
                     })}
-                </p>
+                </p></div>
               <Link className="Link" to="/">Home</Link>
             </div>
         );
-    }
-    componentDidMount() {
-        this.props.getPlans();
     }
 }
 
